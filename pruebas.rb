2bad830ae4a640de7890bc2184c77024a8ad679f
Alan Alfonso Argotte López <alan.argotte69@gmail.com>
2025-12-06 00:08:42 -0400

    Created2

--- pruebas.rb
+++ pruebas.rb
@@ -49,6 +49,8 @@ MAPA_DE_STR_A_CLASE = {
     "tijera" => Tijera
 }
 
+JUGADAS = [Piedra, Papel, Tijera]
+
 class Manual < Estrategia
     def prox()
         print "Inserte la próxima jugada: "
@@ -88,16 +90,71 @@ class Sesgada < Estrategia
     end
 end
 
+class Copiar < Estrategia
+    def prox(jugada_oponente)
+        if jugada_oponente.nil?
+            rndIndex = rand(JUGADAS.length - 1)
+            JUGADAS[rndIndex]
+        else
+            jugada_oponente
+        end
+        
+    end
+end
+
+# def Pensar < Estrategia
+
+# end
+
+class Partida
+    @@nombre_jugador1 = "Pepe"
+    @@estrategia_jugador1 = Uniforme.new
+    @@nombre_jugador2 = "Popo"
+    @@estrategia_jugador2 = Uniforme.new
+    @@modo_de_juego = 0
+    @@modo_de_juego_limite = 4
+    @@puntaje_jugador1 = 0
+    @@puntaje_jugador2 = 0
+    @@rondas = 0
+
+    def iniciar_partida()
+        loop do
+            jugada_1 = @@estrategia_jugador1.prox(JUGADAS)
+            jugada_2 = @@estrategia_jugador2.prox(JUGADAS)
+
+            resultado = jugada_1.puntos(jugada_2)
+
+            if resultado == [1, 0]
+                @@puntaje_jugador1 = @@puntaje_jugador1 + 1
+            elsif resultado == [0, 1]
+                @@puntaje_jugador2 = @@puntaje_jugador2 + 1
+            end
+
+            puts "Resultado de la ronda:\n P1: #{@@puntaje_jugador1}, P2: #{@@puntaje_jugador2}"
+
+            comando = gets.chomp.downcase
+            if comando == "salir"
+                break
+            end
+        end
+    end
+
+end
+
 mapa_de_pesos = { Piedra => 100, Papel => 0, Tijera => 0 }
 
-jugada1 = Piedra.new
-puts "#{jugada1}"
-jugada2 = Tijera.new
-p jugada1.puntos(jugada2)
-estrategia1 = Manual.new
-# p estrategia1.prox
-estrategia2 = Uniforme.new
-lista = [Piedra, Papel, Tijera]
-p estrategia2.prox(lista)
-estrategia3 = Sesgada.new
-p estrategia3.prox(mapa_de_pesos)
+partida = Partida.new
+
+partida.iniciar_partida()
+
+# jugada1 = Piedra.new
+# puts "#{jugada1}"
+# jugada2 = Tijera.new
+# p jugada1.puntos(jugada2)
+# estrategia1 = Manual.new
+# # p estrategia1.prox
+# estrategia2 = Uniforme.new
+# lista = [Piedra, Papel, Tijera]
+# p estrategia2.prox(lista)
+# estrategia3 = Sesgada.new
+# p estrategia3.prox(mapa_de_pesos)
